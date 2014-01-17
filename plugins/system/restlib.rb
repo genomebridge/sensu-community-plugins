e12dea10c6c6484f4ac7327ff8560dde68c1f42a
bernzbroad <dbernick@broadinstitute.org>
2014-01-17 16:55:54 -0500

    change

--- plugins/system/restlib.rb
+++ plugins/system/restlib.rb
@@ -2,8 +2,13 @@ def get_job_id(node_id, basicpass, gateway)
 	require 'rest-client'
 	begin
 		response = RestClient.post "https://"+basicpass+":x@"+gateway+"/cmi-overlord-external/api/docker/node/"+node_id+"/jobInfo", {:accept => :json}      
-		JSON.parse(response)
+		jsonres = JSON.parse(response)
+		if jsonres.empty?
+			JSON.parse('{"id":"Unknown","task":"UnknownTask"}')
+		else
+			jsonres
+		end	
 	rescue => e
-		JSON.parse('{"currentJobId":"Unknown","task":"UnknownTask"}')
+		JSON.parse('{"id":"Unknown","task":"UnknownTask"}')
 	end
 end
